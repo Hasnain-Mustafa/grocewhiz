561209818e57cbb5796a7d0da459bdd0e4f82eae
Muhammad Hasnain Mustafa <hasnainmustafa839@gmail.com>
2024-06-08 20:19:54 +0500

    Added and Configured vite-plugin-pwa

---
+++ client/vite.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico"],
+      manifest: {
+        name: "GroceWhiz",
+        short_name: "GroceWhiz",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "logo 64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "logo 192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "logo 512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "logo 512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
